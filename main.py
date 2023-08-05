c0927853693967473b45e91ee3c96205bc09f94d
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-08-05 23:33:34 +0700

    awal

---
+++ main.py
@@ -0,0 +1,27 @@
+""""
+Aplikasi deteksi gempa terkini
+"""
+
+
+def ekstraksi_data():
+    """
+    Tanggal: 04 Agustus 2023
+    Waktu: 18:48:38 WIB
+    Magnitudo: 6.0
+    Kedalaman: 10 km
+    Lokasi: 0.21 LS - 125.03 BT
+    Pusat gempak: Pusat gempa berada di laut 117 km Tenggara Bolaang Mongondo Timur
+    Dirasakan (Skala MMI): III - IV Bone Bolango, III - IV Gorontalo, III Manado, III Tomohon, III Tondano, III Minahasa Utara, III Bolaang Mongondow Selatan, III Minahasa Tenggara, III Bolaang Mongondow Timur, III Kotamobagu, III Bitung, III Kab. Gorontalo, II-III Luwuk, II-III Kab. Gorontalo Utara, II-III Banggai Kepulauan, II Ampana
+    :return:
+    """
+    pass
+
+
+def tamilkan_data(result):
+    pass
+
+
+if __name__== '__main__':
+    print('Aplikasi utama')
+    result=ekstraksi_data()
+    tamilkan_data(result)
