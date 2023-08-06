78ca43c2d3bf125c60a0fb0c2191acf3a800c148
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-08-06 21:30:27 +0700

    Finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """"
 Aplikasi deteksi gempa terkini
+MODULARISASI DENGAN FUNCTION
 """
 
 
@@ -14,11 +15,28 @@ def ekstraksi_data():
     Dirasakan (Skala MMI): III - IV Bone Bolango, III - IV Gorontalo, III Manado, III Tomohon, III Tondano, III Minahasa Utara, III Bolaang Mongondow Selatan, III Minahasa Tenggara, III Bolaang Mongondow Timur, III Kotamobagu, III Bitung, III Kab. Gorontalo, II-III Luwuk, II-III Kab. Gorontalo Utara, II-III Banggai Kepulauan, II Ampana
     :return:
     """
-    pass
+    hasil = dict()
+    hasil['tanggal'] = '04 Agustus 2023'
+    hasil['waktu'] = '18:48:38 WIB'
+    hasil['magnitudo'] = 6.0
+    hasil['lokasi'] = { 'ls':0.21, 'bt':125.03 }
+    hasil['pusat'] = 'Pusat gempa berada di laut 117 km Tenggara Bolaang Mongondo Timur'
+    hasil['dirasakan'] = 'Dirasakan (Skala MMI): III - IV Bone Bolango, III - IV Gorontalo, III Manado, III Tomohon, III Tondano, III Minahasa Utara, III Bolaang Mongondow Selatan, III Minahasa Tenggara, III Bolaang Mongondow Timur, III Kotamobagu, III Bitung, III Kab. Gorontalo, II-III Luwuk, II-III Kab. Gorontalo Utara, II-III Banggai Kepulauan, II Ampana'
+
+    print(hasil)
+    return hasil
+
 
 
 def tamilkan_data(result):
-    pass
+    print('Gempa Terakhir Berdasarkan BMKG')
+    print(f"tanggal {result['tanggal']}")
+    print (f"waktu {result['waktu']}")
+    print (f"magnitudo {result['magnitudo']}")
+    print (f"lokasi: LS={result ['lokasi']['ls']}, BT={result ['lokasi']['bt']}")
+    print (f"pusat {result['pusat']}")
+    print (f"dirasakan {result['dirasakan']}")
+
 
 
 if __name__== '__main__':
