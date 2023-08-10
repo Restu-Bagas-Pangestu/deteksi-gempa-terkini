6b0f0ac248f31e94d829a70074cf56235daaad2c
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-08-10 23:46:51 +0700

    Finish

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
-""""
+"""
 Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
 """
-
-
-def ekstraksi_data():
-    """
-    Tanggal: 04 Agustus 2023
-    Waktu: 18:48:38 WIB
-    Magnitudo: 6.0
-    Kedalaman: 10 km
-    Lokasi: 0.21 LS - 125.03 BT
-    Pusat gempak: Pusat gempa berada di laut 117 km Tenggara Bolaang Mongondo Timur
-    Dirasakan (Skala MMI): III - IV Bone Bolango, III - IV Gorontalo, III Manado, III Tomohon, III Tondano, III Minahasa Utara, III Bolaang Mongondow Selatan, III Minahasa Tenggara, III Bolaang Mongondow Timur, III Kotamobagu, III Bitung, III Kab. Gorontalo, II-III Luwuk, II-III Kab. Gorontalo Utara, II-III Banggai Kepulauan, II Ampana
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '04 Agustus 2023'
-    hasil['waktu'] = '18:48:38 WIB'
-    hasil['magnitudo'] = 6.0
-    hasil['lokasi'] = { 'ls':0.21, 'bt':125.03 }
-    hasil['pusat'] = 'Pusat gempa berada di laut 117 km Tenggara Bolaang Mongondo Timur'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): III - IV Bone Bolango, III - IV Gorontalo, III Manado, III Tomohon, III Tondano, III Minahasa Utara, III Bolaang Mongondow Selatan, III Minahasa Tenggara, III Bolaang Mongondow Timur, III Kotamobagu, III Bitung, III Kab. Gorontalo, II-III Luwuk, II-III Kab. Gorontalo Utara, II-III Banggai Kepulauan, II Ampana'
-    print(hasil)
-
-    return hasil
-
-
-
-def tamilkan_data(result):
-    print('Gempa Terakhir Berdasarkan BMKG')
-    print(f"tanggal {result['tanggal']}")
-    print (f"waktu {result['waktu']}")
-    print (f"magnitudo {result['magnitudo']}")
-    print (f"lokasi: LS={result ['lokasi']['ls']}, BT={result ['lokasi']['bt']}")
-    print (f"pusat {result['pusat']}")
-    print (f"dirasakan {result['dirasakan']}")
-
+import gempa_terkini
 
 if __name__== '__main__':
     print('Aplikasi utama')
-    result=ekstraksi_data()
-    tamilkan_data(result)
+    result=gempa_terkini.ekstraksi_data()
+    gempa_terkini.tamilkan_data(result)
+
+
+"""
+    
+"""
